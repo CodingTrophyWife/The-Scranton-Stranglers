5a35a2bd049f4620ea94a525bb7d84fa7231ab7b
Cyndi Fosmark <cmfosmark@gmail.com>
2023-04-24 08:27:22 -0700

    updates to index.js file

--- index.js
+++ index.js
@@ -1,73 +1,121 @@
+const { writeFile } = require("fs").promises;
+const generator = require("./util/generateHtml");
 const inquirer = require("inquirer");
-const fs = require('fs');
-const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const generateHtml = require("./util/generateHtml");
 
-
-const answers =[];
-
-const promptQuestions = [
+const team = [];
+const baseQuestions = [
     {
-      type: 'list',
-      message: 'Who would you like to add?',
-      name: 'role',
-      choices: [
-        'Manager',
-        'Engineer',
-        'Intern',
-      ]
-    },{
-      type: 'input',
-      message: `What is the employee name?`,
-      name: 'name',
-    },{
-      type: 'input',
-      message: `What is the employee ID number?`,
-      name: 'id'
-    },{
-      type: 'input',
-      message: `What is the employee email?`,
-      name: 'email'
-    }]
+        name: "name",
+        message:"What is their name?",
+        type: "input"
+    },
+    {
+        name: "id",
+        message: "What is their employee number?",
+        type: "input"
+    },
+    {
+        name: "email",
+        message: "What is their Email?",
+        type: "input",
+    }
+];
 
-    function newEmployee(){
-        inquirer.prompt(promptQuestions)
-        .then(function ({name, id, email, role}) {
-          let extra = '';
-          if (role === 'Manager') {
-              extra = 'office number';
-          } else if (role === 'Engineer') {
-              extra = 'Github username';
-          } else {
-              extra = 'school';
-          }
+function askQuestion() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Who would you like to add?",
+            name: 'question',
+            choices: ['Manager','Engineer','Intern','Finish']
+        }
+    ]).then(answers => {
+        switch (answers.question) {
+            case "Manager":
+                baseQuestions.push(
+                    {
+                        name: "office",
+                        message: "What is their office number?",
+                        type: "input"
+                    }
+                );
+                addManager(baseQuestions);
+                break;
+            case "Engineer":
+                baseQuestions.push(
+                    {
+                        name: "username",
+                        message: "What is their github username?",
+                        type: "input"
+                    }
+                );
+                addEngineer(baseQuestions);
+                break;
+            case "Intern":
+                baseQuestions.push(
+                    {
+                        name: "school",
+                        message: "What is their school?",
+                        type: "input"
+                    }
+                );
+                addIntern(baseQuestions);
+                break;
+            default:
+                console.log("Finished building team, generating html");
+                writeFile("index.html", generator(team))
+                .then(() => console.log("Successfully wrote file"))
+                .catch((err) => console.error(err));
+                break;
+        };
+    });
+}
 
-        inquirer.prompt([{
-            message: `What is the employee's ${extra}?`,
-            name: 'extra',
-            },{
-            message: 'Would you like to add another employee?',
-            name: 'newEmployee',
-            type: 'confirm',
-            }])
-            
-            .then(function({extra, newEmployee}) {
-                let emp;
-                if (role === 'Manager') {
-                    emp = new Manager(name, id, email, extra);
-                } else if (role === 'Engineer') {
-                    emp = new Engineer(name, id, email, extra);
-                } else if(role === 'Intern'){
-                    emp = new Intern(name, id, email, extra);
-                }
-                handleAnswers.push(emp);
-                console.log(handleAnswers)
-                if (newEmployee === true) {
-                    createEmployee();
-                } else {
-                    finished(handleAnswers);
-                };
-            })
-          })}; 
+function addManager(questions) {
+    inquirer.prompt(questions)
+    .then(answers => {
+        const name = answers.name;
+        const id = answers.id;
+        const email = answers.email;
+        const office = answers.office;
+        const member = new Manager(name, id, email, office);
+        team.push(member);
+        baseQuestions.pop();
+        askQuestion();
+    });  
+}
+
+function addEngineer(questions) {
+    inquirer.prompt(questions)
+    .then(answers => {
+        const name = answers.name;
+        const id = answers.id;
+        const email = answers.email;
+        const username = answers.username;
+        const member = new Engineer(name, id, email, username);
+        team.push(member);
+        baseQuestions.pop();
+        askQuestion();
+    });    
+}
+
+function addIntern(questions) {
+    inquirer.prompt(questions)
+    .then(answers => {
+        const name = answers.name;
+        const id = answers.id;
+        const email = answers.email;
+        const school = answers.school;
+        const member = new Intern(name, id, email, school);
+        team.push(member);
+        baseQuestions.pop();
+        askQuestion();
+    });   
+}
+
+askQuestion();
+    
